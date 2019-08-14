4dd4ba271029086f37e556efb5b7d0582fbe9203
chic-page-6502 <zacharytq@gmail.com>
2019-08-14 01:33:42 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.detect{|c| c.sort == @word.sort}
+  end
